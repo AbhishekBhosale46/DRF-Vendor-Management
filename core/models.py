213579b863f127eec176ef31f8c631c2b30a39eb
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2023-12-10 22:42:30 +0530

    fix: modified models.py

--- core/models.py
+++ core/models.py
@@ -2,11 +2,10 @@ from django.db import models
 import uuid
 
 class Vendor(models.Model):
-    vendor_code = models.UUIDField(
-        default=uuid.uuid4,
+    vendor_code = models.CharField(
+        max_length=50,
         unique=True,
         primary_key=True,
-        editable=False
     )
     name = models.CharField(max_length=255)
     contact_details = models.TextField()
@@ -20,11 +19,10 @@ class Vendor(models.Model):
         return f"{self.vendor_code} {self.name}"
 
 class PurchaseOrder(models.Model):
-    po_number = models.UUIDField(
-        default=uuid.uuid4,
+    po_number = models.CharField(
+        max_length=50,
         unique=True,
         primary_key=True,
-        editable=False
     )
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
@@ -32,9 +30,9 @@ class PurchaseOrder(models.Model):
     items = models.JSONField()
     quantity = models.IntegerField()
     status = models.CharField(max_length=20)
-    quality_rating = models.FloatField(null=True)
-    issue_date = models.DateTimeField(auto_now_add=True)
-    acknowledgment_date = models.DateTimeField(null=True)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.po_number}"
